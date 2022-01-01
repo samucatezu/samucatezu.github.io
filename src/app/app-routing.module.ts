1021ca99da8871af3ad26167c42fc3690339446f
Samuel Almeida <barbosasamuel24@gmail.com>
2021-12-31 21:34:35 -0300

    code refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
-import { DashComponent } from './dash/dash.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
+import { layoutComponent } from "./Layout/layout.component";
 
-const routes: Routes = [{ path: 'dashboard', component: DashComponent }];
+const routes: Routes = [{ path: "layout", component: layoutComponent }];
 
 @NgModule({
   declarations: [],
