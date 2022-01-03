d588288cb5eab84c78b3ceda4ac9abef62800fed
Samuel Almeida <barbosasamuel24@gmail.com>
2022-01-03 19:33:08 -0300

    CSS updates

--- src/app/Layout/layout.component.ts
+++ src/app/Layout/layout.component.ts
@@ -1,10 +1,10 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
-import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, ViewChild } from "@angular/core";
+import { MatSidenav } from "@angular/material/sidenav";
+import { BreakpointObserver } from "@angular/cdk/layout";
 @Component({
-  selector: 'app-layout',
-  templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss'],
+  selector: "app-layout",
+  templateUrl: "./layout.component.html",
+  styleUrls: ["./layout.component.scss"],
 })
 export class layoutComponent {
   @ViewChild(MatSidenav)
@@ -13,12 +13,12 @@ export class layoutComponent {
   constructor(private observer: BreakpointObserver) {}
 
   ngAfterViewInit() {
-    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+    this.observer.observe(["(max-width: 2000px)"]).subscribe((res) => {
       if (res.matches) {
-        this.sidenav.mode = 'over';
+        this.sidenav.mode = "over";
         this.sidenav.close();
       } else {
-        this.sidenav.mode = 'side';
+        this.sidenav.mode = "side";
         this.sidenav.open();
       }
     });
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -12,7 +12,7 @@ export class ProfileComponent {
   constructor(private observer: BreakpointObserver) {}
 
   ngAfterViewInit() {
-    this.observer.observe(["(max-width: 800px)"]).subscribe((res) => {
+    this.observer.observe(["(max-width: 2000px)"]).subscribe((res) => {
       if (res.matches) {
         this.sidenav.mode = "over";
         this.sidenav.close();
